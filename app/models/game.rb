ea6d0cb4b6a9fa8110cf12f623d383b1a346117b
togo-mori <togo.mori@di-v.co.jp>
2020-07-31 18:44:50 +0900

    test8

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   has_many :histories
+  validates :title, :maker, :series, :hard, :year, presence: true
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,5 @@
 class History < ApplicationRecord
   belongs_to :game
   has_one_attached :image
+  validates :date, :text, presence: true
 end
