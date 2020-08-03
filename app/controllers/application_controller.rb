6feb137fc9ea1edd6fd31410c21519319f70e9f2
togo-mori <togo.mori@di-v.co.jp>
2020-08-03 09:39:17 +0900

    user verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :mylogger_test
+
   class MyLogger < Logger
     include LoggerSilence
     include ActiveSupport::LoggerThreadSafeLevel
   end
 
-  before_action :mylogger_test
   private
 
   def mylogger_test
@@ -14,5 +17,11 @@ class ApplicationController < ActionController::Base
       mylogger.info("action = #{action_name}")
       mylogger.error("controler#action = #{controller_name}##{action_name}")
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
