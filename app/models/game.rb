a73d56974cd6d82b7f6341402c8b472d966fbe81
togo-mori <togo.mori@di-v.co.jp>
2020-08-05 10:40:38 +0900

    database reset

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,2 @@
 class Game < ApplicationRecord
-  has_many :histories
-  has_many :users
-  validates :title, :maker, :series, :hard, :year, presence: true
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,2 @@
 class History < ApplicationRecord
-  belongs_to :game
-  belongs_to :user
-  has_one_attached :image
-  validates :date, :text, presence: true
 end
