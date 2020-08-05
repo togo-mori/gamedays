a5eb306d8aa6c15f95913809b0dc6e9a6f2581ec
togo-mori <togo.mori@di-v.co.jp>
2020-08-05 11:05:29 +0900

    0805-1

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+  has_many :histories
+  has_many :users
+  validates :title, presence: true
+  validates :maker, presence: true
+  validates :series, presence: true
+  validates :hard, presence: true
+  validates :year, presence: true
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,10 @@
 class History < ApplicationRecord
+  belongs_to :user
+  belongs_to :game
+  has_one_attached :image
+  validates :date, presence: true
+  validates :text, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
