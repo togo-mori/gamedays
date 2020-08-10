aeace989e0ae7049796d70ff3e1d49ae870ed2c1
togo-mori <togo.mori@di-v.co.jp>
2020-08-10 09:58:08 +0900

    model test code

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  before do
+    @game = FactoryBot.build(:game)
+  end
+
+  it "タイトルが空だと登録できない" do
+    @game.title = ""
+    @game.valid?
+    expect(@game.errors.full_messages).to include("Title can't be blank")  
+  end
+
+  it "メーカーが空だと登録できない" do
+    @game.maker = ""
+    @game.valid?
+    expect(@game.errors.full_messages).to include("Maker can't be blank")  
+  end
+  it "シリーズが空だと登録できない" do
+    @game.series = ""
+    @game.valid?
+    expect(@game.errors.full_messages).to include("Series can't be blank")  
+  end
+  it "ハードが空だと登録できない" do
+    @game.hard = ""
+    @game.valid?
+    expect(@game.errors.full_messages).to include("Hard can't be blank")  
+  end
+  it "年が空だと登録できない" do
+    @game.year = ""
+    @game.valid?
+    expect(@game.errors.full_messages).to include("Year can't be blank")  
+  end
+
+  
+  
   
 end
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
+  before do
+    @history = FactoryBot.build(:history)
+    @history.image = fixture_file_upload('public/images/test.png')
+  end
+  
+
+  it "dateが空では登録できない" do
+    @history.date = nil
+    @history.valid?
+    expect(@history.errors.full_messages).to include("Date can't be blank")  
+  end
+
+  it "textとimageとdateが存在すれば登録できる" do
+    expect(@history).to be_valid  
+  end
+
+  it "text空でもimageが存在すれば登録できる" do
+    @history.text = nil
+    expect(@history).to be_valid  
+  end
+
+  it "imageがからでもtextが存在すれば登録できる" do
+    @history.image = nil
+    expect(@history).to be_valid  
+  end
+
+  it "textとimageが空では登録できない" do
+    @history.text = nil
+    @history.image = nil
+    @history.valid?
+    expect(@history.errors.full_messages).to include("Text can't be blank")  
+  end
+  
+  
+  
+  
   
 end
