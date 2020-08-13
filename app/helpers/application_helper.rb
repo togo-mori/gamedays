91ee69e3006785ec79205c5570ed213690ae3b5f
togo-mori <togo.mori@di-v.co.jp>
2020-08-13 11:10:15 +0900

    helper years

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def years(games)
+    html = ''
+    games.order(year: "DESC").pluck(:year).uniq.each do |year|
+      html += link_to(year, year_games_path(year: year), controller: "games", action: "year", class: "year")
+    end
+    return raw(html)
+  end
 end
